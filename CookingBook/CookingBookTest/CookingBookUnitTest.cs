1232d371b7cd9783bbe553e6b904917a5ec4704e
Joy <s-han.ji@lwtech.edu>
2021-12-05 16:24:18 -0800

    Add test for Instruction model

--- CookingBook/CookingBookTest/CookingBookUnitTest.cs
+++ CookingBook/CookingBookTest/CookingBookUnitTest.cs
@@ -7,7 +7,7 @@ namespace CookingBookTest
     public class CookingBookUnitTest
     {
         [Fact]
-        public void IngredientTest1()
+        public void IngredientFactTest()
         {
             Ingredient ingredient = new Ingredient();
             ingredient.IngredientID = 1;
@@ -21,16 +21,43 @@ namespace CookingBookTest
 
         [Theory]
         [InlineData(1, 3, "Mint")]
-        public void IngredientTest2(int IngredientID, int RecipeID, string ingredientString)
+        public void IngredientTheoryTest(int ingredientID, int recipeID, string ingredientString)
         {
             Ingredient ingredient = new Ingredient();
-            ingredient.IngredientID = IngredientID;
-            ingredient.RecipeID = RecipeID;
+            ingredient.IngredientID = ingredientID;
+            ingredient.RecipeID = recipeID;
             ingredient.ingredient = ingredientString;
 
             Assert.Equal(1, ingredient.IngredientID);
             Assert.Equal(3, ingredient.RecipeID);
             Assert.Equal("Mint", ingredient.ingredient);
+        }
+
+        [Fact]
+        public void InstructionFactTest()
+        {
+            Instruction instruction = new Instruction();
+            instruction.InstructionID = 1;
+            instruction.RecipeID = 3;
+            instruction.InstructionText = "Add one tea spoon";
+
+            Assert.Equal(1, instruction.InstructionID);
+            Assert.Equal(3, instruction.RecipeID);
+            Assert.Equal("Add one tea spoon", instruction.InstructionText);
+        }
+
+        [Theory]
+        [InlineData(1, 3, "Add one tea spoon")]
+        public void InstructionTheoryTest(int instructionID, int recipeID, string instructionString)
+        {
+            Instruction instruction = new Instruction();
+            instruction.InstructionID = instructionID;
+            instruction.RecipeID = recipeID;
+            instruction.InstructionText = instructionString;
+
+            Assert.Equal(1, instruction.InstructionID);
+            Assert.Equal(3, instruction.RecipeID);
+            Assert.Equal("Add one tea spoon", instruction.InstructionText);
         }
 
     }
