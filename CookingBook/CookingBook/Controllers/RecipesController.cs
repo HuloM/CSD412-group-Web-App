e4246b518d966ddf2f833f98c3a4d987d8b699df
spencerhirata <spencerhirata@gmail.com>
2021-12-06 03:27:58 -0800

    Search page functionality and API integration finished. Further adjustments necessary for the Search View

--- CookingBook/CookingBook/Controllers/RecipesController.cs
+++ CookingBook/CookingBook/Controllers/RecipesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -26,6 +27,102 @@ namespace CookingBook.Controllers
                 .Include(r => r.Instructions)
                 .Include(r => r.Ingredients)
                 .ToListAsync());
+        }
+
+        // GET: Recipes/Search/
+        public async Task<IActionResult> Search(string query)
+        {
+            IEnumerable<CookingBook.Models.Recipe> recipes = new List<CookingBook.Models.Recipe>();
+            recipes = recipes.Concat(await getDBInformation(query)).ToList();
+            recipes = recipes.Concat(await getAPIInformation(query)).ToList();
+            return View(recipes);
+        }
+
+        // Helper function
+        public async Task<IEnumerable<CookingBook.Models.Recipe>> getAPIInformation(string query )
+        {
+            IEnumerable<CookingBook.Models.Recipe> recipes = new List<CookingBook.Models.Recipe>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&q=" + query),
+                Headers =
+            {
+                { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+                { "x-rapidapi-key", "de25df93c4mshf438aee8873b819p11c9b0jsnd5d5969cc347" },
+            },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                // Create object from text json response
+                var jsonConversion = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(body);
+
+                // Iterate through list of results
+
+                foreach (var result in jsonConversion.results)
+                {
+                    // Add to the list of recipes
+                    recipes = recipes.Append(new Recipe
+                    {
+                        Name = result.ContainsKey("name") ? result.name.ToString() : null,
+                        TotalTime = result.ContainsKey("total_time_minutes") ? (!String.IsNullOrEmpty(result.total_time_minutes.ToString()) && int.TryParse(result.total_time_minutes.ToString(), out int test) ? int.Parse(result.total_time_minutes.ToString()) : 0) : 0,
+                        DateCreated = result.ContainsKey("created_at") ? new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(int.Parse(result.created_at.ToString())).ToLocalTime() : DateTime.Now,
+                        Instructions = result.ContainsKey("instructions") ? new Models.Instruction
+                        {
+                            InstructionText = ((Func<string>)(() =>
+                            {
+                                string instructions = "";
+                                foreach (var instruction in result.instructions)
+                                {
+                                    instructions += instruction.display_text.ToString() + "\n";
+                                }
+                                return instructions;
+                            }))()
+                        } : new Models.Instruction
+                        {
+                            InstructionText = "None Provided",
+                        },
+                        Ingredients = result.ContainsKey("sections") && result.sections[0].ContainsKey("components") ? new Models.Ingredient
+                        {
+                            ingredient = ((Func<string>)(() =>
+                            {
+                                string ingredients = "";
+                                int section = 0;
+                                foreach (var sections in result.sections)
+                                {
+                                    ingredients += "Section " + section + ": \n";
+                                    foreach (var ingredient in sections.components)
+                                    {
+                                        ingredients += ingredient.raw_text.ToString() + "\n";
+                                    }
+                                    ingredients += "\n";
+                                }
+
+                                return ingredients;
+                            }))()
+                        } : new Models.Ingredient
+                        {
+                            ingredient = "None Provided",
+                        }
+                    }).ToList();
+                }
+                return recipes;
+            }
+        }
+
+        // Helper function
+        public async Task<IEnumerable<CookingBook.Models.Recipe>> getDBInformation(string query) {
+            IEnumerable<CookingBook.Models.Recipe> recipes = await _context.Recipe.Where(p =>
+            p.Name.Contains(query) || p.Ingredients.ingredient.Contains(query))
+            .OrderBy(i => i.Name)
+            .ThenBy(i => i.Ingredients.ingredient)
+            .ToListAsync();
+
+            return recipes;
         }
 
         // GET: Recipes/Details/5
